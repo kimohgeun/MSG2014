25bb54ea0d182f863dc2daf4ed5ed908becc7c38
kog <dhrmstl@naver.com>
2014-05-31 20:54:41 +0900

    05.31

--- Ohgeun/MoDoo/gen/com/example/modoo/R.java
+++ Ohgeun/MoDoo/gen/com/example/modoo/R.java
@@ -21,16 +21,37 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int main=0x7f020001;
-        public static final int splash=0x7f020002;
+        public static final int cscs=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int jsjs=0x7f020002;
+        public static final int main=0x7f020003;
+        public static final int splash=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int BtnCalltoSub=0x7f080000;
+        public static final int LinearLayout1=0x7f080002;
+        public static final int action_settings=0x7f08000f;
+        public static final int csb1=0x7f080003;
+        public static final int csb2=0x7f080004;
+        public static final int csb3=0x7f080005;
+        public static final int js1b1=0x7f080006;
+        public static final int js1b2=0x7f080007;
+        public static final int js1b3=0x7f080008;
+        public static final int js2b1=0x7f080009;
+        public static final int js2b2=0x7f08000a;
+        public static final int js2b3=0x7f08000b;
+        public static final int js3b1=0x7f08000c;
+        public static final int js3b2=0x7f08000d;
+        public static final int js3b3=0x7f08000e;
+        public static final int textView1=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int splash=0x7f030001;
+        public static final int cs=0x7f030001;
+        public static final int js1=0x7f030002;
+        public static final int js2=0x7f030003;
+        public static final int js3=0x7f030004;
+        public static final int splash=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- Ohgeun/MoDoo/src/com/example/modoo/MainActivity.java
+++ Ohgeun/MoDoo/src/com/example/modoo/MainActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -13,7 +17,22 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         
         startActivity(new Intent(this, SplashActivity.class));
-    }
+    	Button btnCallMain = (Button) findViewById(R.id.BtnCalltoSub);
+		btnCallMain.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v)
+			{
+				Log.i("onClick", "START");
+				Intent intentSubActivity = 
+						new Intent(MainActivity.this, CsActivity.class);
+				startActivity(intentSubActivity);
+			}
+		
+		});
+	
+	}
+ 
 
 
     @Override
