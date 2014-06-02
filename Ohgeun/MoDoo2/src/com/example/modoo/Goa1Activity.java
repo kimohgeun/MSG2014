b43e64e2961e5bc39047bf0c62d109e5f5e720ae
kimohgeun <kog1059@gmail.com>
2014-06-02 12:29:26 +0900

    modify 2

---
+++ Ohgeun/MoDoo2/src/com/example/modoo/Goa1Activity.java
@@ -0,0 +1,17 @@
+package com.example.modoo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Goa1Activity extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.goa1);
+	    // TODO Auto-generated method stub
+	    
+	}
+
+}
